41876bc14fd5a398ab7fad68a3b4480665a462a3
Mario Hugo Marcelo Corte <84399997+mariocorte@users.noreply.github.com>
2026-04-23 20:25:25 -0300

    Set fondo.png as site background

--- depesca/lib/main.dart
+++ depesca/lib/main.dart
@@ -9,10 +9,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/fondo.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    blurRadius: 8,
+                    color: Colors.black54,
+                    offset: Offset(1, 1),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
